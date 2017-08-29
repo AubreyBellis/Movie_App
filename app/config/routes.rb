3de193e7646bbbcfd4fb8b267e847a84460e72d3
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-29 19:01:40 -0400

    more updates

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'films#index'
   resources :films
-  root to: "films#index"
-  # get     '/films',          to: 'films#index'
-  # get     '/films/new',      to: 'films#new'
-  # post    '/films',          to: 'films#create'
-
-  # get     '/films/:id',      to: 'films#show', as: "film"
-  # get     '/films/:id/edit', to: 'films#edit'
-  # put     '/films/:id',      to: 'films#update'
-  # delete  '/films/:id',      to: 'films#destroy'
 end
